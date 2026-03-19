714a5c282cdd24a9a5dc244d7568da32b7459fcf
Emman <emmanueldavid@visailabs.com>
2026-03-19 15:23:58 +0530

    Corrected nginx running templates before certs, removed unwaned validation

--- deploy/nginx/entrypoint/40-render-config.sh
+++ deploy/nginx/entrypoint/40-render-config.sh
@@ -2,10 +2,10 @@
 set -eu
 
 APP_DOMAIN=${APP_DOMAIN:-_}
-TEMPLATE=/etc/nginx/templates/site-http.conf.template
+TEMPLATE=/etc/nginx/templates-custom/site-http.conf.template
 
 if [ "$APP_DOMAIN" != "_" ] && [ -f "/etc/letsencrypt/live/$APP_DOMAIN/fullchain.pem" ]; then
-    TEMPLATE=/etc/nginx/templates/site-https.conf.template
+    TEMPLATE=/etc/nginx/templates-custom/site-https.conf.template
 fi
 
 export APP_DOMAIN
